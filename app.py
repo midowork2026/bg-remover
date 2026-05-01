02bc40ec0ee2e2a713b66d2143753faa5f3e6337
midowork2026 <midowork2026@gmail.com>
2026-05-01 18:35:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,6 @@ def upload():
     img_base64 = base64.b64encode(img_io.getvalue()).decode('utf-8')
     return render_template('index.html', result_image=img_base64)
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
