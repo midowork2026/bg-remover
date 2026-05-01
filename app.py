e5097124f03c81119424af9f23e9a429050891b1
midowork2026 <midowork2026@gmail.com>
2026-05-01 23:02:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 from flask import Flask, render_template, request
-from rembg import remove
-from PIL import Image
-import io
+import requests
 import base64
 import os
 
-app = Flask(__name__)
+app = Flask(_name_)
 
-# الصفحة الرئيسية
+# 🔥 حط API KEY هنا
+API_KEY = "T8PX6VMNgTofTAD72S4AZZPx"
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-# رفع الصورة ومعالجتها
 @app.route('/upload', methods=['POST'])
 def upload():
     try:
@@ -22,25 +21,25 @@ def upload():
         if not file:
             return render_template('index.html', error="ارفع صورة")
 
-        # 🔥 تقليل حجم الصورة (مهم عشان السيرفر ما يقعش)
-        input_image = Image.open(file.stream).convert('RGBA')
-        input_image.thumbnail((500, 500))
+        response = requests.post(
+            'https://api.remove.bg/v1.0/removebg',
+            files={'image_file': file},
+            data={'size': 'auto'},
+            headers={'X-Api-Key': API_KEY},
+        )
 
-        output_image = remove(input_image)
+        if response.status_code != requests.codes.ok:
+            return render_template('index.html', error="خطأ في المعالجة")
 
-        img_io = io.BytesIO()
-        output_image.save(img_io, 'PNG')
-        img_io.seek(0)
-
-        img_base64 = base64.b64encode(img_io.getvalue()).decode('utf-8')
+        img_base64 = base64.b64encode(response.content).decode('utf-8')
 
         return render_template('index.html', result_image=img_base64)
 
     except Exception as e:
         print("ERROR:", e)
-        return render_template('index.html', error="حصل خطأ، جرب صورة أصغر")
+        return render_template('index.html', error="حصل خطأ")
 
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
